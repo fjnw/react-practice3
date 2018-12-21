4e31fc15f23e6235859198a15f7564eb16eb3821
Jamal <faizanjamal2018@u.northwestern.edu>
2018-12-20 23:05:12 -0600

    added input with handler to update state with char split.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import ValidationComponent from "./Components/ValidationComponent/ValidationComponent";
+import CharComponent from "./Components/CharComponent/CharComponent";
+
 
 class App extends Component {
+
+  state = {
+    text: []
+  }
+
+  inputUpdateHandler = (event) => {
+    // const val = [...event.target.value]
+    const val = event.target.value.split("")
+
+    this.setState({text: val}, function () {
+      console.log(this.state.text)
+    })
+  }
+
   render() {
+
+    
+
+
     return (
       <div className="App">
+        <input 
+          type="text" 
+          onChange={(event) => this.inputUpdateHandler(event)} />
 
+        
+        
       </div>
     );
   }
---
+++ src/Components/ValidationComponent/ValidationComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ValidationComponent = () => {
+
+  return (
+    <div>
+      <input />
+    </div>
+  )
+
+};
+
+export default ValidationComponent;
